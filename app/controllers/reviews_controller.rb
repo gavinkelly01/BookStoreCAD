3f59f6b70b72e34421e8016569a7525f10d4d567
Gavin Kelly <124623076+gavinkelly01@users.noreply.github.com>
2024-12-16 20:33:53 +0000

    Update reviews_controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,31 +1,18 @@
 class ReviewsController < ApplicationController
-  before_action :set_book, only: [:index, :create]
+  before_action :set_book
 
-  # GET /books/:book_id/reviews
   def index
     @reviews = @book.reviews
     render json: @reviews
   end
 
-  # POST /books/:book_id/reviews
-  def create
-    @review = @book.reviews.new(review_params)
-    if @review.save
-      render json: @review, status: :created
-    else
-      render json: { errors: @review.errors.full_messages }, status: :unprocessable_entity
-    end
-  end
-
+  # Other actions follow the same pattern
+  
   private
 
   def set_book
-    @book = Book.find_by(id: params[:book_id])
-    render json: { error: 'Book not found' }, status: :not_found if @book.nil?
-  end
-
-  def review_params
-    params.require(:review).permit(:content, :rating)
+    @book = Book.find(params[:book_id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "Book not found" }, status: :not_found
   end
 end
-
