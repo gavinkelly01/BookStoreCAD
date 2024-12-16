de7bb3449ce72c514381c4a157c39096e14ece26
Gavin Kelly <124623076+gavinkelly01@users.noreply.github.com>
2024-12-16 20:41:18 +0000

    Update reviews_controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,13 @@
-# app/controllers/reviews_controller.rb
-
 class ReviewsController < ApplicationController
   before_action :set_book, only: [:create]
 
+  # POST /books/:book_id/reviews
   def create
     @review = @book.reviews.new(review_params)
     if @review.save
       render json: @review, status: :created
     else
+      # Log the errors to help debug
       render json: { errors: @review.errors.full_messages }, status: :unprocessable_entity
     end
   end
@@ -21,6 +21,7 @@ class ReviewsController < ApplicationController
   end
 
   def review_params
+    # Ensure that we permit the 'content' and 'rating' within the 'review' key
     params.require(:review).permit(:content, :rating)
   end
 end
